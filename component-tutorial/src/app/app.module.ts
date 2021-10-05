0e64fb5cee10891e7d453e974b5e4d31ab77507e
123JackCole <123jack.cole@gmail.com>
2021-10-05 14:35:00 -0700

    reactive form practice

--- component-tutorial/src/app/app.module.ts
+++ component-tutorial/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
+import { SquarePipe } from './custom-pipe/square.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    BasicHighlightDirective,
+    BetterHighlightDirective,
+    SquarePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
